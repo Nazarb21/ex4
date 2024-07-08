a37b47eb81488edc6f4ca7385ac153a8bea7abcf
Nazarb21 <146764633+Nazarb21@users.noreply.github.com>
2024-07-08 12:13:45 +0300

    Add files via upload

---
+++ routes/userRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const { registerUser } = require('../controllers/userController');
+
+const router = express.Router();
+
+router.post('/users', registerUser);
+
+module.exports = router;
+
+const { getUser } = require('../controllers/userController');
+
+router.get('/users/:id', getUser);
+
+module.exports = router;
+
+const { updateUserDetails } = require('../controllers/userController');
+
+router.put('/users/:id', updateUserDetails);
+
+module.exports = router;
